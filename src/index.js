3da8c119980e7e53deee7dbc02ba535a385d455e
hubkrecz <107886221+hubkrecz@users.noreply.github.com>
2022-07-13 17:52:18 +0200

    counter logic done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const decreaseButton = document.querySelector("#decrease");
+const resetButton = document.querySelector("#reset");
+const increaseButton = document.querySelector("#increase");
+let textInNumber = null;
+
+const decreaseFunction = () => {
+  textInNumber = document.querySelector("#number").textContent;
+  textInNumber = textInNumber - 1;
+  document.querySelector("#number").textContent = textInNumber;
+};
+
+const resetFunction = () => {
+  document.querySelector("#number").textContent = "0";
+};
+
+const increaseFunction = () => {
+  textInNumber = document.querySelector("#number").textContent;
+  textInNumber = textInNumber - 0 + 1;
+  document.querySelector("#number").textContent = textInNumber;
+};
+
+decreaseButton.addEventListener("click", decreaseFunction);
+resetButton.addEventListener("click", resetFunction);
+increaseButton.addEventListener("click", increaseFunction);
